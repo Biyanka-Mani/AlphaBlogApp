9c8e0d77c18c40e98dc8443590227f506e1b36d5
Biyanka-Mani <biyanka167@gmail.com>
2024-05-15 13:33:21 +0530

    Add show feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'remote/home'
+  get 'play/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,6 +9,11 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about',to:'pages#about'
+
+
+  resources :articles,only: [:show]
+  
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
