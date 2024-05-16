efed916c79871524fb4800401e3c11ba9dd9ba12
Biyanka-Mani <biyanka167@gmail.com>
2024-05-16 12:50:59 +0530

    Validation meassages and flash messaging features added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,10 +6,23 @@ class ArticlesController < ApplicationController
         @articles=Article.all
     end
     def new
+        @article=Article.new
     end
     def create 
        @article=Article.new(params.require(:article).permit(:title, :Description))
-       @article.save
-       redirect_to article_path(@article)
+       
+       if @article.save
+        flash[:notice]="Artice is saved successfully"
+        redirect_to article_path(@article)
+       else
+        render :new, status: :unprocessable_entity
+       end
+
+    end
+    def edit
+        @article=Article.new()
+    end
+    def update
+
     end
 end
