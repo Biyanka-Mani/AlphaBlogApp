252eb650fe6d9d01da5cebfc202998b52d1c7c00
Biyanka-Mani <biyanka167@gmail.com>
2024-05-27 15:59:08 +0530

    Add User - Articles Assosications

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
     end
     def create 
        @article=Article.new(articles_params)
-       
+       @article.user=User.first
        if @article.save
         flash[:notice]="Article is saved successfully"
         redirect_to article_path(@article)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_070952) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_27_095348) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "Description"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_070952) do
     t.datetime "updated_at", null: false
     t.datetime "starting"
     t.datetime "ending"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
