59dbd5d7b67b37e82cff061a43a15e450f5c7499
Biyanka-Mani <biyanka167@gmail.com>
2024-05-27 17:54:57 +0530

    secure password added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save{self.email=email.downcase}
     has_many :articles
     validates :username,presence: true,
                         uniqueness: { case_sensitive: false },
@@ -8,4 +9,5 @@ VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
                         uniqueness: { case_sensitive: false },
                                 length: {maximum:100},
                                     format: { with:VALID_EMAIL_REGEX}
+has_secure_password
 end
